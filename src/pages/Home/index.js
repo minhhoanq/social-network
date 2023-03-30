53d80db1ad4b1513c1851d48dfa799f2e341fb82
minhhoanq <mh.tranminhhoang@gmail.com>
2023-03-30 18:17:09 +0700

    Use Component Icons

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,7 @@ import Button from '~/components/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEarthAsia, faFaceAngry, faKeyboard, faLanguage, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import Menu from '~/components/Popper/Menu';
+import { CommentIcon, EmojiIcon, HeartIcon, MesageIcon, SavedIcon, ShareIcon } from '~/components/Icons';
 
 const cx = classNames.bind(styles);
 
@@ -122,12 +123,7 @@ function Home() {
                                                             <div className={cx('profile_btn_mess')}>
                                                                 <button className={cx('btn_mess')}>
                                                                     <Link className={cx('btn_mess-link')}>
-                                                                        <svg
-                                                                            xmlns="http://www.w3.org/2000/svg"
-                                                                            viewBox="0 0 512 512"
-                                                                        >
-                                                                            <path d="M256.55 8C116.52 8 8 110.34 8 248.57c0 72.3 29.71 134.78 78.07 177.94 8.35 7.51 6.63 11.86 8.05 58.23A19.92 19.92 0 0 0 122 502.31c52.91-23.3 53.59-25.14 62.56-22.7C337.85 521.8 504 423.7 504 248.57 504 110.34 396.59 8 256.55 8zm149.24 185.13l-73 115.57a37.37 37.37 0 0 1-53.91 9.93l-58.08-43.47a15 15 0 0 0-18 0l-78.37 59.44c-10.46 7.93-24.16-4.6-17.11-15.67l73-115.57a37.36 37.36 0 0 1 53.91-9.93l58.06 43.46a15 15 0 0 0 18 0l78.41-59.38c10.44-7.98 24.14 4.54 17.09 15.62z" />
-                                                                        </svg>
+                                                                        ,<MesageIcon />
                                                                         Message
                                                                     </Link>
                                                                 </button>
@@ -174,113 +170,22 @@ function Home() {
                                         <div className={cx('desc_wrapper')}>
                                             <button className={cx('desc_icon', 'desc_reaction')}>
                                                 <div className={cx('desc_inner', 'desc_inner_reaction', 'clickTym')}>
-                                                    {/* <svg
-                                                        aria-label="Like"
-                                                        className={cx('x1lliihq x1n2onr6')}
-                                                        color="rgb(38, 38, 38)"
-                                                        fill="rgb(38, 38, 38)"
-                                                        height="24"
-                                                        role="img"
-                                                        viewBox="0 0 24 24"
-                                                        width="24"
-                                                    >
-                                                        <title>Like</title>
-                                                        <path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path>
-                                                    </svg> */}
-
-                                                    <svg
-                                                        aria-label="Unlike"
-                                                        className={cx('x1lliihq x1n2onr6')}
-                                                        color="rgb(255, 48, 64)"
-                                                        fill="rgb(255, 48, 64)"
-                                                        height="24"
-                                                        role="img"
-                                                        viewBox="0 0 48 48"
-                                                        width="24"
-                                                    >
-                                                        <title>Unlike</title>
-                                                        <path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
-                                                    </svg>
+                                                    <HeartIcon />
                                                 </div>
                                             </button>
                                             <button className={cx('desc_icon')}>
                                                 <div className={cx('desc_inner')}>
-                                                    <svg
-                                                        aria-label="Comment"
-                                                        className={cx('x1lliihq x1n2onr6')}
-                                                        color="rgb(38, 38, 38)"
-                                                        fill="rgb(38, 38, 38)"
-                                                        height="24"
-                                                        role="img"
-                                                        viewBox="0 0 24 24"
-                                                        width="24"
-                                                    >
-                                                        <title>Comment</title>
-                                                        <path
-                                                            d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z"
-                                                            fill="none"
-                                                            stroke="currentColor"
-                                                            strokeLinejoin={'round'}
-                                                            strokeWidth={'2'}
-                                                        ></path>
-                                                    </svg>
+                                                    <CommentIcon />
                                                 </div>
                                             </button>
                                             <button className={cx('desc_icon')}>
                                                 <div className={cx('desc_inner')}>
-                                                    <svg
-                                                        aria-label="Share Post"
-                                                        className={cx('x1lliihq x1n2onr6')}
-                                                        color="rgb(38, 38, 38)"
-                                                        fill="rgb(38, 38, 38)"
-                                                        height="24"
-                                                        role="img"
-                                                        viewBox="0 0 24 24"
-                                                        width="24"
-                                                    >
-                                                        <title>Share Post</title>
-                                                        <line
-                                                            fill="none"
-                                                            stroke="currentColor"
-                                                            strokeLinejoin={'round'}
-                                                            strokeWidth={'2'}
-                                                            x1="22"
-                                                            x2="9.218"
-                                                            y1="3"
-                                                            y2="10.083"
-                                                        ></line>
-                                                        <polygon
-                                                            fill="none"
-                                                            points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
-                                                            stroke="currentColor"
-                                                            strokeLinejoin={'round'}
-                                                            strokeWidth={'2'}
-                                                        ></polygon>
-                                                    </svg>
+                                                    <ShareIcon />
                                                 </div>
                                             </button>
                                             <button className={cx('desc_icon', 'desc_save')}>
                                                 <div className={cx('desc_inner')}>
-                                                    <svg
-                                                        aria-label="Save"
-                                                        className={cx('x1lliihq x1n2onr6')}
-                                                        color="rgb(38, 38, 38)"
-                                                        fill="rgb(38, 38, 38)"
-                                                        height="24"
-                                                        role="img"
-                                                        viewBox="0 0 24 24"
-                                                        width="24"
-                                                    >
-                                                        <title>Save</title>
-                                                        <polygon
-                                                            fill="none"
-                                                            points="20 21 12 13.44 4 21 4 3 20 3 20 21"
-                                                            stroke="currentColor"
-                                                            strokeLinecap={'round'}
-                                                            strokeLinejoin={'round'}
-                                                            strokeWidth={'2'}
-                                                        ></polygon>
-                                                    </svg>
+                                                    <SavedIcon />
                                                 </div>
                                             </button>
                                         </div>
@@ -330,19 +235,7 @@ function Home() {
                                                 <input type={'text'} placeholder={'Add a comment...'} />
                                             </button>
                                             <div className={cx('icon_comment')}>
-                                                <svg
-                                                    aria-label="Emoji"
-                                                    className={cx('x1lliihq x1n2onr6')}
-                                                    color="rgb(142, 142, 142)"
-                                                    fill="rgb(142, 142, 142)"
-                                                    height="13"
-                                                    role="img"
-                                                    viewBox="0 0 24 24"
-                                                    width="13"
-                                                >
-                                                    <title>Emoji</title>
-                                                    <path d="M15.83 10.997a1.167 1.167 0 1 0 1.167 1.167 1.167 1.167 0 0 0-1.167-1.167Zm-6.5 1.167a1.167 1.167 0 1 0-1.166 1.167 1.167 1.167 0 0 0 1.166-1.167Zm5.163 3.24a3.406 3.406 0 0 1-4.982.007 1 1 0 1 0-1.557 1.256 5.397 5.397 0 0 0 8.09 0 1 1 0 0 0-1.55-1.263ZM12 .503a11.5 11.5 0 1 0 11.5 11.5A11.513 11.513 0 0 0 12 .503Zm0 21a9.5 9.5 0 1 1 9.5-9.5 9.51 9.51 0 0 1-9.5 9.5Z"></path>
-                                                </svg>
+                                                <EmojiIcon />
                                             </div>
                                         </div>
                                     </div>
