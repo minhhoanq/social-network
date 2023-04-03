09bfa67f4433a87577a6806f2d9d56b4235bdc15
minhhoanq <mh.tranminhhoang@gmail.com>
2023-04-03 23:51:46 +0700

    Icons and Warnings

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,7 @@ import Button from '~/components/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEarthAsia, faFaceAngry, faKeyboard, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import Menu from '~/components/Popper/Menu';
-import { CommentIcon, EmojiIcon, HeartIcon, MesageIcon, SavedIcon, ShareIcon } from '~/components/Icons';
+import { CommentIcon, EmojiIcon, HeartIcon, MesageIcon, MorePostIcon, SavedIcon, ShareIcon } from '~/components/Icons';
 import Search from '~/components/Search';
 
 const cx = classNames.bind(styles);
@@ -143,20 +143,7 @@ function Home() {
                                     </div>
                                     <button className={cx('header_options')}>
                                         <div className={cx('options_svg')}>
-                                            <svg
-                                                aria-label="More options"
-                                                className={cx('_ab6-')}
-                                                color="rgb(142, 142, 142)"
-                                                fill="rgb(38, 38, 38)"
-                                                height="24"
-                                                role="img"
-                                                viewBox="0 0 24 24"
-                                                width="24"
-                                            >
-                                                <circle cx="12" cy="12" r="1.5"></circle>
-                                                <circle cx="6" cy="12" r="1.5"></circle>
-                                                <circle cx="18" cy="12" r="1.5"></circle>
-                                            </svg>
+                                            <MorePostIcon />
                                         </div>
                                     </button>
                                 </div>
